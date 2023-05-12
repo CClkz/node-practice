a05e0806839d9e7b64863685f6bbbc1102eb05e9
smj <sunmingjie@winupon.com>
2023-05-12 16:06:22 +0800

    feat: buffer

--- server/buffer.js
+++ server/buffer.js
@@ -1,6 +1,8 @@
 /**
- * buffer:缓冲,内存区域
+ * buffer:字节数组|缓冲|内存区域
  */
+
+const { buffer } = require('stream/consumers')
 
 // console.log(Object.keys(Buffer));
 // [
@@ -19,6 +21,17 @@
 
 const buf = Buffer.from('Hey!')
 console.log('buf', buf, buf.length, buf[0], buf[1], buf[2], buf[3])
+console.log('buf string', buf.toString())
 
 const buf1 = Buffer.alloc(4)
 console.log('buf1', buf1, buf1.length, buf1[0], buf1[1], buf1[2], buf1[3])
+console.log('buf1 string', buf1.toString())
+buf1.write('a')
+console.log('buf1 string', buf1.toString()) // a
+buf1.write('bcdef')
+console.log('buf1 string', buf1.toString()) // bcde
+let bufCopy = Buffer.alloc(4)
+buf1.copy(bufCopy)
+console.log('bufCopy', bufCopy.toString())
+
+// buffer.slice 不是副本，跟随原始buffer变化，这点与Array.slice不同
