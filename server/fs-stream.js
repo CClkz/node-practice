f79115cf026c59527906f7c61c381b43f7bc970a
cclkz <1964450091@qq.com>
2024-05-07 00:38:36 +0800

    perf: 回顾&完善注释

--- server/fs-stream.js
+++ server/fs-stream.js
@@ -12,17 +12,18 @@ const fs = require('fs')
 
 const server = http.createServer(function (req, res) {
   console.log('req.url', req.url)
-  console.log('res', res)
   console.log('-----------------------------')
-  // 典型案例，读取文件
-  const stream = fs.createReadStream(__dirname + '/test.txt')
-  // 获取来源流，并将其通过管道传输到目标流，（返回目标流，即res）
-  stream.pipe(res)
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  // // 典型案例，读取文件
+  // const stream = fs.createReadStream(__dirname + '/test.txt')
+  // // 获取来源流，并将其通过管道传输到目标流，（返回目标流，即res）
+  // stream.pipe(res)
 
-  // fs.readFile(__dirname + '/test.txt', (err, data) => {
-  //   console.log('file data', data.toString())
-  //   res.end(data)
-  // })
+  fs.readFile(__dirname + '/test.txt', (err, data) => {
+    // data为buffer数据
+    console.log('file data', data.toString())
+    res.end(data)
+  })
 })
 server.listen(3000, () => {
   console.log('server run on port 3000')
--- server/stream.js
+++ server/stream.js
@@ -20,21 +20,31 @@ const Stream = require('stream')
 
 // 可读流
 const readableStream = new Stream.Readable({
-  read() {}
+  read() {
+    // read触发的三种情况
+    // 1. 直接调用readableStream.read()
+    // 2. 监听data
+    // 3. 推数据入流，readableStream.push('')
+    console.log('read')
+  }
 })
 
 // 可写流
 const writableStream = new Stream.Writable()
 writableStream._write = (chunk, encoding, next) => {
-  console.log(chunk.toString())
+  console.log('_write', chunk.toString())
   next()
 }
 readableStream.pipe(writableStream)
 
+// 异步任务,被加到缓冲区，后面统一处理，只触发一次read
+
 readableStream.push('hello')
 readableStream.push('world')
+console.log('before write')
+// 同步任务
 writableStream.write('write')
 
 setTimeout(() => {
   writableStream.end()
-}, 500)
+}, 1500)
